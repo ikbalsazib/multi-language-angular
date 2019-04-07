0e6fbe420d49184663b47df2b238b94b394ff75a
Md Iqbal Hossen <ikbal.sazib@.com>
2019-04-07 20:05:20 +0600

    multi language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+export function setupTranslateFactory(
+  service: TranslateService): Function {
+  return () => service.use('en');
+}
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {TranslateService} from "./translate.service";
+import { TranslatePipe } from './translate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    TranslateService,
+    TranslatePipe,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: setupTranslateFactory,
+      deps: [ TranslateService ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
